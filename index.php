a900b62c798b59ac452773ea0ae3b945b5efc78c
Achmoye <achmoye@gmail.com>
2023-09-26 19:46:58 +0200

    Small improvments

--- index.php
+++ index.php
@@ -7,7 +7,16 @@
    
     <?php include "./navbar.html" ?>
 
-<p> test </p> 
+<h1>Welcome to Path of Crafting</h1>
+<ul>
+    <li class="flavour">A crafting app using very simplified system of Path of Exile.
+    The true system, similar to craftofexile.com, will be made in a new application.
+
+    </li><li class="flavour"> This is a little project made for learning, with no framework.</li>
+</ul>
+
+<hr>
+
 
 </body>
 </html>
